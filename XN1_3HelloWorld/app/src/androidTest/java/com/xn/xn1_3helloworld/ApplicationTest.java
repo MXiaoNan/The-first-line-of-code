1723d249bd335e6ec7fa2dd6afbe07c0f7c9c280
xiaonan <xiaonan9421@gmail.com>
2016-07-31 10:00:43 +0800

    update

---
+++ XN1_3HelloWorld/app/src/androidTest/java/com/xn/xn1_3helloworld/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.xn.xn1_3helloworld;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
